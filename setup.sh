8cfd3bb2a2130dff2a7ff5b4d1c9e4bea34897fe
Mars <a11223d@gmail.com>
2017-11-04 15:36:50 -0400

    startserver changed

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ declare -a servers=($2 $3 $4 $5 $6)
 
 for server in "${servers[@]}"
 do
-{
+
 ssh ${username}@${server} /bin/bash <<EOF
 sudo apt-get update
 sudo apt-get install -y screen python-pip python-dev vim
 sudo pip install --upgrade pip
 sudo pip install tensorflow
+git clone https://github.com/forestliurui/EECS598-Assignment02.git
 cp -r /proj/michigan-bigdata-PG0/assignments/assignment2/* .
 EOF
-} &
-done
+scp startserver.py ${username}@${server}:~
 
-wait
+done
